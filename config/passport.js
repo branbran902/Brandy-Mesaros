38266b415b138fe7a6cd23e63060cc535f9369de
paulj <paul.jacobs@journey.ai>
2021-03-14 01:50:49 -0700

    use sequelize not mongoose

--- config/passport.js
+++ config/passport.js
@@ -120,6 +120,7 @@ passport.use(new SnapchatStrategy({
 /**
  * Sign in with Facebook.
  */
+
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_ID,
   clientSecret: process.env.FACEBOOK_SECRET,
@@ -160,7 +161,7 @@ passport.use(new FacebookStrategy({
           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
           done(err);
         } else {
-          const user = new User();
+          const user = User();
           user.email = profile._json.email;
           user.facebook = profile.id;
           user.tokens.push({ kind: 'facebook', accessToken });
@@ -301,118 +302,6 @@ const googleStrategyConfig = new GoogleStrategy({
 });
 passport.use('google', googleStrategyConfig);
 refresh.use('google', googleStrategyConfig);
-
-/**
- * Sign in with LinkedIn.
- */
-passport.use(new LinkedInStrategy({
-  clientID: process.env.LINKEDIN_ID,
-  clientSecret: process.env.LINKEDIN_SECRET,
-  callbackURL: `${process.env.BASE_URL}/auth/linkedin/callback`,
-  scope: ['r_liteprofile', 'r_emailaddress'],
-  passReqToCallback: true
-}, (req, accessToken, refreshToken, profile, done) => {
-  if (req.user) {
-    User.findOne({ linkedin: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        req.flash('errors', { msg: 'There is already a LinkedIn account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, (err, user) => {
-          if (err) { return done(err); }
-          user.linkedin = profile.id;
-          user.tokens.push({ kind: 'linkedin', accessToken });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.picture = user.profile.picture || profile.photos[3].value;
-          user.save((err) => {
-            if (err) { return done(err); }
-            req.flash('info', { msg: 'LinkedIn account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ linkedin: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
-        if (err) { return done(err); }
-        if (existingEmailUser) {
-          req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with LinkedIn manually from Account Settings.' });
-          done(err);
-        } else {
-          const user = new User();
-          user.linkedin = profile.id;
-          user.tokens.push({ kind: 'linkedin', accessToken });
-          user.email = profile.emails[0].value;
-          user.profile.name = profile.displayName;
-          user.profile.picture = user.profile.picture || profile.photos[3].value;
-          user.save((err) => {
-            done(err, user);
-          });
-        }
-      });
-    });
-  }
-}));
-
-/**
- * Sign in with Instagram.
- */
-passport.use(new InstagramStrategy({
-  clientID: process.env.INSTAGRAM_ID,
-  clientSecret: process.env.INSTAGRAM_SECRET,
-  callbackURL: '/auth/instagram/callback',
-  passReqToCallback: true
-}, (req, accessToken, refreshToken, profile, done) => {
-  if (req.user) {
-    User.findOne({ instagram: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        req.flash('errors', { msg: 'There is already an Instagram account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
-      } else {
-        User.findById(req.user.id, (err, user) => {
-          if (err) { return done(err); }
-          user.instagram = profile.id;
-          user.tokens.push({ kind: 'instagram', accessToken });
-          user.profile.name = user.profile.name || profile.displayName;
-          user.profile.picture = user.profile.picture || profile._json.data.profile_picture;
-          user.profile.website = user.profile.website || profile._json.data.website;
-          user.save((err) => {
-            req.flash('info', { msg: 'Instagram account has been linked.' });
-            done(err, user);
-          });
-        });
-      }
-    });
-  } else {
-    User.findOne({ instagram: profile.id }, (err, existingUser) => {
-      if (err) { return done(err); }
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-      const user = new User();
-      user.instagram = profile.id;
-      user.tokens.push({ kind: 'instagram', accessToken });
-      user.profile.name = profile.displayName;
-      // Similar to Twitter API, assigns a temporary e-mail address
-      // to get on with the registration process. It can be changed later
-      // to a valid e-mail address in Profile Management.
-      user.email = `${profile.username}@instagram.com`;
-      user.profile.website = profile._json.data.website;
-      user.profile.picture = profile._json.data.profile_picture;
-      user.save((err) => {
-        done(err, user);
-      });
-    });
-  }
-}));
-
 
 /**
  * Login Required middleware.
--- models/User.js
+++ models/User.js
@@ -1,90 +1,90 @@
-// const bcrypt = require('bcrypt');
-// const crypto = require('crypto');
-
-// const userSchema = new mongoose.Schema({
-//   email: { type: String, unique: true },
-//   password: String,
-//   passwordResetToken: String,
-//   passwordResetExpires: Date,
-//   emailVerificationToken: String,
-//   emailVerified: Boolean,
-
-//   snapchat: String,
-//   facebook: String,
-//   twitter: String,
-//   google: String,
-//   github: String,
-//   instagram: String,
-//   linkedin: String,
-//   steam: String,
-//   twitch: String,
-//   quickbooks: String,
-//   tokens: Array,
-
-//   profile: {
-//     name: String,
-//     gender: String,
-//     location: String,
-//     website: String,
-//     picture: String
-//   }
-// }, { timestamps: true });
-
-// /**
-//  * Password hash middleware.
-//  */
-// userSchema.pre('save', function save(next) {
-//   const user = this;
-//   if (!user.isModified('password')) { return next(); }
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) { return next(err); }
-//     bcrypt.hash(user.password, salt, (err, hash) => {
-//       if (err) { return next(err); }
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-// /**
-//  * Helper method for validating user's password.
-//  */
-// userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-//     cb(err, isMatch);
-//   });
-// };
-
-// /**
-//  * Helper method for getting user's gravatar.
-//  */
-// userSchema.methods.gravatar = function gravatar(size) {
-//   if (!size) {
-//     size = 200;
-//   }
-//   if (!this.email) {
-//     return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-//   }
-//   const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-//   return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-// };
-
-// const User = mongoose.model('User', userSchema);
-
-// module.exports = User;
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
+const sequelize = new Sequelize(process.env.DB,
+                                process.env.USER,
+                                process.env.PASSWORD,
                                 {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
+                                    host: process.env.HOST,
+                                    port: process.env.PORT,
                                     dialect: 'postgres',
                                     dialectOptions: {
                                         ssl: process.env.DB_SSL == "true"
                                     }
                                 });
+
+const userSchema = sequelize.define('user', {
+  email: { type: Sequelize.STRING, unique: true },
+  password: Sequelize.STRING,
+  passwordResetToken: Sequelize.STRING,
+  passwordResetExpires: Sequelize.DATE,
+  emailVerificationToken: Sequelize.STRING,
+  emailVerified: Sequelize.BOOLEAN,
+
+  snapchat: Sequelize.STRING,
+  facebook: Sequelize.STRING,
+  twitter: Sequelize.STRING,
+  google: Sequelize.STRING,
+  github: Sequelize.STRING,
+  instagram: Sequelize.STRING,
+  linkedin: Sequelize.STRING,
+  steam: Sequelize.STRING,
+  twitch: Sequelize.STRING,
+  quickbooks: Sequelize.STRING,
+  tokens: Sequelize.STRING,
+}, { timestamps: true });
+
+const profileSchema = sequelize.define('profile', {
+    name: Sequelize.STRING,
+    gender: Sequelize.STRING,
+    location: Sequelize.STRING,
+    website: Sequelize.STRING,
+    picture: Sequelize.STRING,
+});
+
+userSchema.hasOne(profileSchema);
+profileSchema.belongsTo(userSchema);
+
+/**
+ * Password hash middleware.
+ */
+userSchema.addHook('beforeSave', (next) => {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+userSchema.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+
+/**
+ * Helper method for getting user's gravatar.
+ */
+userSchema.gravatar = function gravatar(size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
 const Person = sequelize.define('Person', {
     firstName: {
         type: Sequelize.STRING,
@@ -97,5 +97,6 @@ const Person = sequelize.define('Person', {
 });
 module.exports = {
     sequelize: sequelize,
-    Person: Person
+    Person: Person,
+    User: userSchema,
 };

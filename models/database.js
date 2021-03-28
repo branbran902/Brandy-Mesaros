eda0381fc4e56600af697be41dab9f64d4d1d8eb
paulj <paul.jacobs@journey.ai>
2021-03-28 12:50:51 -0600

    update postgres ssl environment variable

--- models/database.js
+++ models/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.POSTGRES_DB,
                                     port: process.env.POSTGRES_PORT,
                                     dialect: 'postgres',
                                     dialectOptions: {
-                                        ssl: process.env.POSTGRES_DB_SSL == "true"
+                                        ssl: process.env.POSTGRES_SSL == "true"
                                     }
                                 },{
                                   logging: console.log,  
